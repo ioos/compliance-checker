a22b4883fe28ba9ec2f922b1896281055a1da2c8
Dave Foster <dfoster@asascience.com>
2014-02-28 14:08:03 -0500

    Disabled udunits for now

--- compliance_checker/cf/util.py
+++ compliance_checker/cf/util.py
@@ -2,7 +2,10 @@ import os
 import os.path
 import itertools
 from lxml import etree
-from udunitspy import Unit, UdunitsError, Converter
+try:
+    from udunitspy import Unit, UdunitsError, Converter
+except ImportError:
+    pass #disabled as CF is not working and udunits won't install on centos/rhel yet
 from netCDF4 import Dimension, Variable
 from pkgutil import get_data
 
