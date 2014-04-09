4d1171df0e6393826866cf66a5371a0cadd6b44f
DanielJMaher <DanielJMaher@gmail.com>
2014-04-09 12:47:03 -0400

    Reverted Minor changes in suite.py and util.py

--- compliance_checker/cf/util.py
+++ compliance_checker/cf/util.py
@@ -2,10 +2,10 @@ import os
 import os.path
 import itertools
 from lxml import etree
-#try:
-from udunitspy import Unit, UdunitsError, Converter
-#except ImportError:
-    #pass #disabled as CF is not working and udunits won't install on centos/rhel yet
+try:
+    from udunitspy import Unit, UdunitsError, Converter
+except ImportError:
+    pass #disabled as CF is not working and udunits won't install on centos/rhel yet
 from netCDF4 import Dimension, Variable
 from pkgutil import get_data
 
