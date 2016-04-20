8e115c5b5815a919f0b4f29c01aeda5f510b331d
Benjamin Adams <ben.adams@rpsgroup.com>
2016-04-20 16:26:34 -0400

    Adds supports for multiple test invocation from CLI
    Allows multiple tests to be specified with the command line by
    separating them by commas without spaces.  Implements #133 by way of
    explicit specifications.

--- cchecker.py
+++ cchecker.py
@@ -5,6 +5,7 @@ import argparse
 import sys
 from compliance_checker.runner import ComplianceChecker, CheckSuite
 from compliance_checker import __version__
+from ordered_set import OrderedSet
 
 
 def main():
@@ -12,14 +13,46 @@ def main():
     check_suite = CheckSuite()
     check_suite.load_all_available_checkers()
 
+    class SupportedTests(argparse.Action):
+        """
+A custom argparse Action to ensure only the supported tests are passed in
+        """
+        def __call__(self, parser, namespace, values, option_string=None):
+            choices_sorted = sorted(check_suite.checkers.keys())
+            choices = set(check_suite.checkers.keys())
+            specified = OrderedSet(values.split(','))
+            arg_diff = specified - choices
+            if len(specified - choices) != 0:
+                err_msg = "Test arguments {} not in valid values of {}".format(list(arg_diff), choices_sorted)
+                raise argparse.ArgumentError(self, err_msg)
+            else:
+                setattr(namespace, self.dest, specified)
+
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--test', '-t', '--test=', '-t=', action='append', help="Select the Checks you want to perform.", choices=sorted(check_suite.checkers.keys()))
-    parser.add_argument('--criteria', '-c', help="Define the criteria for the checks.  Either Strict, Normal, or Lenient.  Defaults to Normal.", nargs='?', default='normal', choices = ['lenient', 'normal', 'strict'])
-    parser.add_argument('--verbose' , '-v', help="Increase output. May be specified up to three times.", action="count")
-    parser.add_argument('-f', '--format', default='text', choices=['text', 'html', 'json'], help='Output format')
-    parser.add_argument('-o', '--output', default='-', action='store', help='Output filename')
-    parser.add_argument('-V', '--version', action='store_true', help='Display the IOOS Compliance Checker version information.')
-    parser.add_argument('dataset_location', nargs='*', help="Defines the location of the dataset to be checked.")
+    parser.add_argument('--test', '-t', '--test=', '-t=', required=True,
+                        #nargs='+',
+                        action=SupportedTests,
+                        help="Select the Checks you want to perform.")
+                        #choices=sorted(check_suite.checkers.keys()))
+
+    parser.add_argument('--criteria', '-c',
+                        help="Define the criteria for the checks.  Either Strict, Normal, or Lenient.  Defaults to Normal.",
+                        nargs='?', default='normal',
+                        choices = ['lenient', 'normal', 'strict'])
+
+    parser.add_argument('--verbose', '-v',
+                        help="Increase output. May be specified up to three times.",
+                        action="count")
+
+    parser.add_argument('-f', '--format', default='text',
+                        choices=['text', 'html', 'json'], help='Output format')
+    parser.add_argument('-o', '--output', default='-', action='store',
+                        help='Output filename')
+    parser.add_argument('-V', '--version', action='store_true',
+                        help='Display the IOOS Compliance Checker version information.')
+    parser.add_argument('dataset_location', nargs='*',
+                        help="Defines the location of the dataset to be checked.")
 
     args = parser.parse_args()
     args.test = args.test or ['acdd']
