a457bbd56ba7045be0cd8189d45f0ee436652d0b
Ryan Maciel <rspams1234@gmail.com>
2018-08-01 10:36:12 -0400

    Fixed issue with get_axis_map using sets instead of lists

--- compliance_checker/cfutil.py
+++ compliance_checker/cfutil.py
@@ -725,9 +725,8 @@ def get_axis_map(ds, variable):
 
     # For example
     # {'x': ['longitude'], 'y': ['latitude'], 't': ['time']}
-    axis_map = defaultdict(set)
+    axis_map = defaultdict(list)
     for coord_name in all_coords:
-
         if is_compression_coordinate(ds, coord_name):
             axis = 'C'
         elif coord_name in times:
@@ -743,12 +742,11 @@ def get_axis_map(ds, variable):
 
         if coord_name in ds.variables[variable].dimensions:
             if coord_name not in axis_map[axis]:
-                axis_map[axis].add(coord_name)
+                axis_map[axis].append(coord_name)
 
         elif coord_name in coordinates:
             if coord_name not in axis_map[axis]:
-                axis_map[axis].add(coord_name)
-
+                axis_map[axis].append(coord_name)
     return axis_map
 
 
