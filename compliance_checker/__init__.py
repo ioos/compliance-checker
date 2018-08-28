c52afe22e4aee3cfefbb4bdb22888c2c16c04c06
Benjamin Adams <ben.adams@rpsgroup.com>
2018-08-28 16:31:55 -0400

    Add docstring to MemoizedDataset

--- compliance_checker/__init__.py
+++ compliance_checker/__init__.py
@@ -10,6 +10,12 @@ except ImportError:
     from functools32 import lru_cache
 
 class MemoizedDataset(Dataset):
+    """
+    A NetCDF dataset which has its get_variables_by_attributes call memoized in
+    order to speed up repeated calls to the function.  This should only really
+    be used against netCDF Datasets opened in 'r' mode, as the attributes should
+    not change upon reading the files.
+    """
     @lru_cache(128)
     def get_variables_by_attributes(self, **kwargs):
         return super(MemoizedDataset,
