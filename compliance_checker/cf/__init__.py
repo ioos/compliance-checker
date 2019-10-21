1c242facc9b057d8c510f709f25c2a691aa28f81
daltonkell <daltonkell13@gmail.com>
2019-10-21 20:48:06 +0000

    Refactor check for dimensionless vertical coordinates and update Appendix D for CF-1.7. Include test for new implementation.

--- compliance_checker/cf/__init__.py
+++ compliance_checker/cf/__init__.py
@@ -4,11 +4,12 @@ from compliance_checker.cf.cf import (
     util,
 )
 
-from compliance_checker.cf.appendix_d import dimless_vertical_coordinates
+from compliance_checker.cf.appendix_d import dimless_vertical_coordinates_1_6, dimless_vertical_coordinates_1_7
 
 __all__ = [
     'CF1_6Check',
     'CF1_7Check',
-    'dimless_vertical_coordinates',
+    'dimless_vertical_coordinates_1_6',
+    'dimless_vertical_coordinates_1_7',
     'util',
 ]
