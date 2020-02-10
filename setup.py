cf8249a1c374ed509a9b49e025e1141b6d15d978
Filipe Fernandes <ocefpaf@gmail.com>
2020-02-10 18:54:47 -0300

    limit to python >=3.5

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     url                  = "https://github.com/ioos/compliance-checker",
     packages             = find_packages(),
     install_requires     = pip_requirements(),
+    python_requires      = "~=3.5",
     tests_require        = ['pytest'],
     classifiers          = [
         'Development Status :: 5 - Production/Stable',
