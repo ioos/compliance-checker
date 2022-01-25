3018981a4808cdbbf755a76ea5020a403b4d9a23
Benjamin Adams <ben.adams@rpsgroup.com>
2022-01-25 14:19:57 -0500

    Fix help blurb mentioning --include-checks

--- cchecker.py
+++ cchecker.py
@@ -112,7 +112,7 @@ def main():
                                     will skip medium and low.  "L" will show
                                     both high and medium priority issues, while
                                     skipping low priority issues.  Cannot be
-                                    used with `-i`/`--include` option.
+                                    used with `-i`/`--include-checks` option.
                                     """
         ),
         action="append",
