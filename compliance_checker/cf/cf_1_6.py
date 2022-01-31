c2086f2dabf68b3c229d7309e9c87b74b33a888a
Benjamin Adams <ben.adams@rpsgroup.com>
2022-01-31 16:38:15 -0500

    Add int16 in addition to short for scale_factor/add_offset comparison

--- compliance_checker/cf/cf_1_6.py
+++ compliance_checker/cf/cf_1_6.py
@@ -146,7 +146,8 @@ class CF1_6Check(CFNCCheck):
                 att.dtype == variable.dtype
             ) or (  # will short-circuit or if first condition is true
                 isinstance(att, (np.float, np.double, float))
-                and variable.dtype in (np.byte, np.short, np.int, int))
+                and variable.dtype in (np.byte, np.short, np.int16, np.int,
+                                       int))
         if not val:
             msgs.append(error_msg)
 
