0bb804148900258cfbe712e3c0a5a78b31b55da0
Leila Belabbassi <leila.baghdadbrahim@gmail.com>
2022-10-12 16:50:10 -0500

    Updated check_compression_gathering
    Related to [issue#956] (https://github.com/ioos/compliance-checker/issues/956)
    Solving:
    - [ ] The values of the associated coordinate variable must be in the range starting with 0 and going up to the product of the compressed dimension sizes minus 1 (CDL index conventions).

--- compliance_checker/cf/cf_1_6.py
+++ compliance_checker/cf/cf_1_6.py
@@ -3287,7 +3287,23 @@ class CF1_6Check(CFNCCheck):
                         compress_var.name, not_in_dims
                     )
                 )
-
+            # IMPLEMENTATION CONFORMANCE 8.2 REQUIRED 3/3
+            # The values of the associated coordinate variable must be in the range 
+            # starting with 0 and going up to the product of the compressed dimension 
+            # sizes minus 1 (CDL index conventions).
+            
+            # Put the the values of the associated coordinate variable into a list
+            coord_list_size = [item.size for item in ds.dimensions.values() if item.name in compress_set]
+            # get the uppper limt of the dimenssion size
+            upper_limit_size = np.prod(coord_list_size) - 1
+            
+            for coord_size in coord_list_size:
+                if coord_size not in range(0,upper_limit_size): 
+                    valid = False
+                    reasoning.append(
+                            'The dimenssion size {} referenced by the commpress attribute is not ' 
+                            'in the range (0, The product of the compressed dimension sizes minus 1)'.format(coord_size)
+                            )
             result = Result(
                 BaseCheck.MEDIUM, valid, self.section_titles["8.2"], reasoning
             )
