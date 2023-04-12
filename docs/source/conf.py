8c072e7818d1cf2cf5f7b54f8aefc19ef02b1191
Filipe Fernandes <ocefpaf@gmail.com>
2023-04-12 10:42:50 -0300

    pre-commit fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,12 +59,10 @@ napoleon_use_rtype = False
 autoclass_content = "both"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {
-    '**': ['versions', 'sidebar-nav-bs']
-}
+html_sidebars = {"**": ["versions", "sidebar-nav-bs"]}
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
