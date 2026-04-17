7af6358fb0ac0b15a9ab6054b794d43b148a8d46
tomaioo <hongsaygio.com@gmail.com>
2026-04-17 05:12:26 -0700

    fix(security): network requests lack explicit timeout, enabling r
    Remote calls in `is_opendap` and `create_DAP_variable_str` do not define timeouts. A slow or non-responsive endpoint can cause the process/thread to block indefinitely, which can be leveraged for denial-of-service in long-running services.
    
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- compliance_checker/protocols/opendap.py
+++ compliance_checker/protocols/opendap.py
@@ -26,7 +26,7 @@ def create_DAP_variable_str(url):
     """
 
     # get dds
-    with urllib.request.urlopen(f"{url}.dds") as resp:
+    with urllib.request.urlopen(f"{url}.dds", timeout=10) as resp:
         _str = resp.read().decode()[8:]
 
     # remove beginning and ending braces, split on newlines
@@ -58,7 +58,7 @@ def is_opendap(url):
         das_url = url + ".das"
 
     try:
-        response = requests.get(das_url, allow_redirects=True)
+        response = requests.get(das_url, allow_redirects=True, timeout=(3, 10))
 
         if "xdods-server" in response.headers:
             return True
@@ -67,4 +67,6 @@ def is_opendap(url):
             return True
     except requests.exceptions.InvalidSchema:
         return False  # not opendap if url + ".das" isn't found
+    except requests.exceptions.Timeout:
+        return False
     return False
